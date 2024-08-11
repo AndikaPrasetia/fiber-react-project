e6e34556df6525d61246bb81f1d8933e6fe72a01
AndikaPrasetia <andikaprasetia@proton.me>
2024-08-11 19:23:05 +0700

    Adding Comments (POST req.)

--- main.go
+++ main.go
@@ -33,24 +33,34 @@ func main() {
 		})
 	})
 
+	// Menambahkan route POST untuk path "/api/todos"
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		// Membuat pointer ke struct Todo
 		todo := &Todo{}
 
+		// Mengurai body request ke dalam struct Todo
 		if err := c.BodyParser(todo); err != nil {
+			// Mengembalikan error jika terjadi kesalahan saat parsing body
 			return err
 		}
 
+		// Memeriksa apakah field Body kosong
 		if todo.Body == "" {
+			// Mengembalikan status 400 dan pesan error jika Body kosong
 			return c.Status(400).JSON(fiber.Map{
 				"error": "Body is required",
 			})
 		}
 
+		// Mengatur ID todo berdasarkan panjang slice todos
 		todo.ID = len(todos) + 1
 
+		// Menambahkan todo baru ke slice todos
 		todos = append(todos, *todo)
+		// Mengembalikan status 201 dan JSON dari todo yang baru dibuat
 		return c.Status(201).JSON(todo)
 	})
+
 	// Menjalankan server pada port 8080 dan log error jika terjadi
 	log.Fatal(app.Listen(":8080"))
 }
