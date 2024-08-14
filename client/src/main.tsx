569a790a9751942f90c82466daf3c2b061e38305
AndikaPrasetia <andikaprasetia@proton.me>
2024-08-14 13:37:05 +0700

    Consume API from React

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider> 
-  </StrictMode>,
+const queryClient = new QueryClient()
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
 )
